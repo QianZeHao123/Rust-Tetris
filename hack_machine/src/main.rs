fe57ad1f9bd63c35a6937b4469fb389feae8ee07
Qian Zehao <qianzehao123@gmail.com>
2024-03-04 22:23:08 +0000

    update main.rs

--- hack_machine/src/main.rs
+++ hack_machine/src/main.rs
@@ -1,16 +1,27 @@
 mod atom_component;
 mod basic_gate;
+pub use atom_component::dff;
 pub use atom_component::nand;
 pub use basic_gate::basic;
+
 fn main() {
-    // test mux_gate
-    println!("mux_gate(0, 1, 0) = {}", basic::mux_gate(0, 1, 0));
-    println!("mux_gate(0, 1, 1) = {}", basic::mux_gate(0, 1, 1));
-    println!("mux_gate(1, 0, 0) = {}", basic::mux_gate(1, 0, 0));
-    println!("mux_gate(1, 0, 1) = {}", basic::mux_gate(1, 0, 1));
-    // test dmux_gate
-    println!("dmux_gate(0, 0) = {:?}", basic::dmux_gate(0, 0));
-    println!("dmux_gate(0, 1) = {:?}", basic::dmux_gate(0, 1));
-    println!("dmux_gate(1, 0) = {:?}", basic::dmux_gate(1, 0));
-    println!("dmux_gate(1, 1) = {:?}", basic::dmux_gate(1, 1));
+    println!("Hack computer simulator");
+    let mut dff = dff::DFlipFlop::new();
+    println!("dff.clock(0, 0) = {}", dff.clock(1, 1));
+    println!("dff.clock(1, 0) = {}", dff.clock(1, 1));
+    println!("dff.clock(0, 1) = {}", dff.clock(0, 1));
+    println!("dff.clock(1, 1) = {}", dff.clock(1, 1));
+    println!("dff.clock(1, 1) = {}", dff.clock(1, 1));
+    println!("dff.clock(1, 1) = {}", dff.clock(1, 1));
+    println!("dff.clock(1, 1) = {}", dff.clock(0, 1));
+    // // test mux_gate
+    // println!("mux_gate(0, 1, 0) = {}", basic::mux_gate(0, 1, 0));
+    // println!("mux_gate(0, 1, 1) = {}", basic::mux_gate(0, 1, 1));
+    // println!("mux_gate(1, 0, 0) = {}", basic::mux_gate(1, 0, 0));
+    // println!("mux_gate(1, 0, 1) = {}", basic::mux_gate(1, 0, 1));
+    // // test dmux_gate
+    // println!("dmux_gate(0, 0) = {:?}", basic::dmux_gate(0, 0));
+    // println!("dmux_gate(0, 1) = {:?}", basic::dmux_gate(0, 1));
+    // println!("dmux_gate(1, 0) = {:?}", basic::dmux_gate(1, 0));
+    // println!("dmux_gate(1, 1) = {:?}", basic::dmux_gate(1, 1));
 }
