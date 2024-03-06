cf1b2b2bf32e37dbf69a618c61c1c31d8dc48720
Qian Zehao <qianzehao123@gmail.com>
2024-03-06 23:07:55 +0000

    add unit test for DFF

--- hack_machine/src/atom_component/dff.rs
+++ hack_machine/src/atom_component/dff.rs
@@ -6,26 +6,6 @@
 // The DFF has one output: the value stored in the memory cell.
 // The DFF has two states: the current state and the next state. The current state is the value stored in the memory cell.
 
-// pub struct DFlipFlop {
-//     current_data_input: u8, // Data input
-//     last_data_input: u8,    // Current state/output
-// }
-// impl DFlipFlop {
-//     pub fn new() -> DFlipFlop {
-//         DFlipFlop {
-//             current_data_input: 0,
-//             last_data_input: 0,
-//         }
-//     }
-//     pub fn clock(&mut self, data_input: u8, clock_input: u8) -> u8 {
-//         if clock_input == 1 {
-//             self.last_data_input = self.current_data_input;
-//             self.current_data_input = data_input;
-//         }
-//         self.last_data_input
-//     }
-// }
-
 // from 0 to 1: clockUp --> Get Input, state = input
 // from 1 to 0: clockDown --> Output, output = state
 pub struct DFlipFlop {
@@ -41,5 +21,20 @@ impl DFlipFlop {
             self.state = input;
         }
         self.state
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_dff() {
+        let mut dff = DFlipFlop::new();
+        assert_eq!(dff.clock(0, 0), 0);
+        assert_eq!(dff.clock(0, 1), 0);
+        assert_eq!(dff.clock(1, 1), 1);
+        assert_eq!(dff.clock(0, 1), 0);
+        assert_eq!(dff.clock(1, 0), 0);
+        assert_eq!(dff.clock(0, 0), 0);
     }
 }
