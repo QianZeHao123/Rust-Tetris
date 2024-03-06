b7f45d4272fd45a80def14cb6ed1e8a7e96b0209
Qian Zehao <qianzehao123@gmail.com>
2024-03-06 23:08:01 +0000

    add unit test

--- hack_machine/src/atom_component/nand.rs
+++ hack_machine/src/atom_component/nand.rs
@@ -4,3 +4,15 @@ pub fn nand_gate(input_a: u8, input_b: u8) -> u8 {
         _ => 1,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_nand_gate() {
+        assert_eq!(nand_gate(0, 0), 1);
+        assert_eq!(nand_gate(0, 1), 1);
+        assert_eq!(nand_gate(1, 0), 1);
+        assert_eq!(nand_gate(1, 1), 0);
+    }
+}
