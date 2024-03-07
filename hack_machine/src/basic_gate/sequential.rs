03c9785e6c7f55823ec63b71e83482f7ef721f14
Qian Zehao <qianzehao123@gmail.com>
2024-03-07 14:50:23 +0000

    add bit register and its unit test

--- hack_machine/src/basic_gate/sequential.rs
+++ hack_machine/src/basic_gate/sequential.rs
@@ -7,7 +7,7 @@ use crate::basic_gate::basic::mux_gate;
 
 pub struct BitRegister {
     dff: dff::DFlipFlop,
-    state: u8
+    state: u8,
 }
 
 // implement BitRegister using DFlipFlop
@@ -15,12 +15,29 @@ impl BitRegister {
     pub fn new() -> BitRegister {
         BitRegister {
             dff: dff::DFlipFlop::new(),
-            state: 0
+            state: 0,
         }
     }
     pub fn clock(&mut self, input: u8, load: u8, clock: u8) -> u8 {
         let new_state = self.dff.clock(input, clock);
         self.state = mux_gate(self.state, new_state, load);
         self.state
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_bit_register() {
+        let mut bit_register = BitRegister::new();
+        println!("bit_register.clock(1, 1, 0) = {}", bit_register.clock(1, 1, 0));
+        println!("bit_register.clock(1, 1, 1) = {}", bit_register.clock(1, 1, 1));
+        println!("bit_register.clock(0, 0, 0) = {}", bit_register.clock(0, 0, 0));
+        println!("bit_register.clock(0, 0, 1) = {}", bit_register.clock(0, 0, 1));
+        println!("bit_register.clock(1, 0, 0) = {}", bit_register.clock(1, 0, 0));
+        println!("bit_register.clock(1, 0, 1) = {}", bit_register.clock(1, 0, 1));
+        println!("bit_register.clock(0, 1, 0) = {}", bit_register.clock(0, 1, 0));
+        println!("bit_register.clock(0, 1, 1) = {}", bit_register.clock(0, 1, 1));
     }
 }
