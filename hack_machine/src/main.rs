32b6f1df30a534c988b4ff90eb16fdbed73a8bce
Qian Zehao <qianzehao123@gmail.com>
2024-03-09 17:54:02 +0000

    update main

--- hack_machine/src/main.rs
+++ hack_machine/src/main.rs
@@ -1,11 +1,19 @@
 mod atom_component;
 mod basic_gate;
-pub use atom_component::dff;
-pub use atom_component::nand;
-pub use basic_gate::basic;
-pub use basic_gate::combinational;
-pub use basic_gate::sequential;
+mod component;
+// pub use atom_component::dff;
+// pub use atom_component::nand;
+// pub use basic_gate::basic;
+// pub use basic_gate::combinational;
+// pub use basic_gate::sequential;
+// pub use basic_gate::add;
+// pub use component::alu::test_alu;
+// pub use component;
 
 fn main() {
     println!("Hack computer simulator");
+    component::alu::test_alu();
+    // alu
+    let result = component::alu::alu([1; 16], [1; 16]);
+    println!("{:?}", result);
 }
