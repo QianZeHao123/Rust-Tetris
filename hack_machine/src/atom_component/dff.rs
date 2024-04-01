ec6af770b1dbbd32cd5a39ab0a25998ad59074fc
Zehao Qian <qianzehao123@gmail.com>
2024-04-01 16:32:31 +0000

    fix the DFF public

--- hack_machine/src/atom_component/dff.rs
+++ hack_machine/src/atom_component/dff.rs
@@ -1,25 +1,25 @@
-struct DFlipFlop {
+pub struct DFlipFlop {
     data_input: i32,
     output_q: i32,
 }
 
 impl DFlipFlop {
-    fn new() -> Self {
+    pub fn new() -> Self {
         DFlipFlop {
             data_input: 0, // 0 represents false
             output_q: 0,   // 0 represents false
         }
     }
 
-    fn set_data_input(&mut self, value: i32) {
+    pub fn set_data_input(&mut self, value: i32) {
         self.data_input = value;
     }
 
-    fn clock_rising_edge(&mut self) {
+    pub fn clock_rising_edge(&mut self) {
         self.output_q = self.data_input;
     }
 
-    fn get_output(&self) -> i32 {
+    pub fn get_output(&self) -> i32 {
         self.output_q
     }
 }
@@ -28,7 +28,7 @@ impl DFlipFlop {
 mod tests {
     use super::*;
     #[test]
-    fn test_dff2() {
+    fn test_dff() {
         let mut dff = DFlipFlop::new();
         dff.set_data_input(1);
         dff.clock_rising_edge();
