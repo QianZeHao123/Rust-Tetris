dc3f97c40b674c29d67f21011fe3cda971c4721e
Zehao Qian <qianzehao123@gmail.com>
2024-04-01 15:50:37 +0000

    update dff implementation

--- hack_machine/src/atom_component/dff.rs
+++ hack_machine/src/atom_component/dff.rs
@@ -1,27 +1,26 @@
-// data flip-flop (DFF) component
-// A DFF is a 1-bit memory cell. It stores a single bit of data and has two outputs: Q and Q' (the inverse of Q).
-// The DFF has two inputs: data input and clock input. The data input is the value to be stored in the memory cell.
-// The clock input is used to control when the data input is stored in the memory cell.
-// When the clock input is 1, the data input is stored in the memory cell. When the clock input is 0, the data input is ignored.
-// The DFF has one output: the value stored in the memory cell.
-// The DFF has two states: the current state and the next state. The current state is the value stored in the memory cell.
-
-// from 0 to 1: clockUp --> Get Input, state = input
-// from 1 to 0: clockDown --> Output, output = state
-
-pub struct DFlipFlop {
-    state: u8,
+struct DFlipFlop {
+    data_input: i32,
+    output_q: i32,
 }
 
 impl DFlipFlop {
-    pub fn new() -> DFlipFlop {
-        DFlipFlop { state: 0 }
-    }
-    pub fn clock(&mut self, input: u8, clock: u8) -> u8 {
-        if clock == 1 {
-            self.state = input;
+    fn new() -> Self {
+        DFlipFlop {
+            data_input: 0, // 0 represents false
+            output_q: 0,   // 0 represents false
         }
-        self.state
+    }
+
+    fn set_data_input(&mut self, value: i32) {
+        self.data_input = value;
+    }
+
+    fn clock_rising_edge(&mut self) {
+        self.output_q = self.data_input;
+    }
+
+    fn get_output(&self) -> i32 {
+        self.output_q
     }
 }
 
@@ -29,13 +28,14 @@ impl DFlipFlop {
 mod tests {
     use super::*;
     #[test]
-    fn test_dff() {
+    fn test_dff2() {
         let mut dff = DFlipFlop::new();
-        assert_eq!(dff.clock(0, 0), 0);
-        assert_eq!(dff.clock(0, 1), 0);
-        assert_eq!(dff.clock(1, 1), 1);
-        assert_eq!(dff.clock(0, 1), 0);
-        assert_eq!(dff.clock(1, 0), 0);
-        assert_eq!(dff.clock(0, 0), 0);
+        dff.set_data_input(1);
+        dff.clock_rising_edge();
+        assert_eq!(dff.get_output(), 1);
+
+        dff.set_data_input(0);
+        dff.clock_rising_edge();
+        assert_eq!(dff.get_output(), 0);
     }
 }
